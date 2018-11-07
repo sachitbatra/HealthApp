79bc531ff90666cef20e3aa63867f58463ba8e76
shreyavshetty <shreyav4@gmail.com>
2018-11-07 21:52:26 +0530

    Added doctors dashboard

---
+++ Frontend/js/update_schedule.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp', ['angularjs-datetime-picker']);
+app.controller('todoCtrl', function($scope) {
+    $scope.todoList = [];
+
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+        $scope.todoInput = "";
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.todoList.push(x);
+        });
+    };
+});
