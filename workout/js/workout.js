9016a6266584888897e07dcdbb0ace4e0bf0dcd8
shreyavshetty <shreyav4@gmail.com>
2018-11-20 22:43:13 +0530

    Added workout templates

---
+++ workout/js/workout.js
@@ -0,0 +1,11 @@
+function SelectCheck(nameSelect)
+{
+   if(nameSelect){
+     selectValue = document.getElementById("sel").value;
+     if(selectValue=="Squats")
+     {
+       divElement = document.getElementById("exinfo");
+       divElement.innerHTML= "This workout dkclvkkvlfddddddn fv klvzv kfjvzn fdzkvldsv flkv ckvnzfdjvd fvkvjv";
+     }
+   }
+}
