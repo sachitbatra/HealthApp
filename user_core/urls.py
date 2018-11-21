b92fd8d0210be038bae2cd07ba03665cc878b396
sanketh96 <sanky.rangreji96@gmail.com>
2018-11-21 13:26:54 +0530

    change in urls

--- user_core/urls.py
+++ user_core/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
-from . import views
+from .views import *
 app_name = 'user_core'
 
 urlpatterns = [
-    path('signed_in/',views.signed_in,name='signed_in'),
-    path('search/',views.search_doctor,name='search'),
-    path('go_to_chat/',views.go_to_chat,name='go_to_chat'),
+    path('signed_in/', signed_in, name='signed_in'),
+    path('search/', search_doctor, name='search'),
+    path('go_to_chat/', go_to_chat, name='go_to_chat'),
 ]
